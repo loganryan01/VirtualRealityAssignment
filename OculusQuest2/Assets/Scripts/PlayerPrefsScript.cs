391f8f7a26462f9dad175e9f2564330644f4b9d0
Logan Ryan <loganryan982@gmail.com>
2021-06-03 16:08:43 +1000

    Add rotation to settings menu

--- OculusQuest2/Assets/Scripts/PlayerPrefsScript.cs
+++ OculusQuest2/Assets/Scripts/PlayerPrefsScript.cs
@@ -5,25 +5,44 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class PlayerPrefsScript : MonoBehaviour
 {
-    string controls;
-
     public TeleportationProvider teleportationProvider;
     public ActionBasedContinuousMoveProvider continuousMoveProvider;
+    public ActionBasedSnapTurnProvider snapTurnProvider;
+    public ActionBasedContinuousTurnProvider continuousTurnProvider;
+
+    public static bool teleportationMovement = false;
+    public static bool continuousMovement = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        controls = PlayerPrefs.GetString("Controls");
+        string controls = PlayerPrefs.GetString("Controls");
+        string rotationControls = PlayerPrefs.GetString("Rotation");
 
         if (controls == "Teleportation")
         {
             teleportationProvider.enabled = true;
             continuousMoveProvider.enabled = false;
+
+            teleportationMovement = true;
         }
         else
         {
             teleportationProvider.enabled = false;
             continuousMoveProvider.enabled = true;
+
+            continuousMovement = true;
+        }
+
+        if (rotationControls == "Snap Turn")
+        {
+            snapTurnProvider.enabled = true;
+            continuousTurnProvider.enabled = false;
+        }
+        else
+        {
+            snapTurnProvider.enabled = false;
+            continuousTurnProvider.enabled = true;
         }
     }
 }
