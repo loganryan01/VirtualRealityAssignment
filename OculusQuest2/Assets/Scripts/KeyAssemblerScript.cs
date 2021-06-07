4e4f8e86afcd1db384fcff536bd68992a0c2143e
TheTastyGravy <thomasdufresne23@gmail.com>
2021-06-07 23:17:22 +1000

    Add prototype for key assembly
    Key assembly consists or attacking the key pieces to sockets, rotating
    the socket blocks, the blocks coming together, and the key being instantiated.
    All of these parts are implemented very roughly and need testing and
    improvement.

---
+++ OculusQuest2/Assets/Scripts/KeyAssemblerScript.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class KeyAssemblerScript : MonoBehaviour
+{
+    //prefab to instantiate when keys are combined
+    public GameObject completeKeyPrefab;
+    //the socket the complete key should be put in
+    public XRSocketInteractor completeKeySocket;
+
+    public GameObject leftBlock;
+    public GameObject rightBlock;
+
+    //the expected rotation of the left block
+    public float leftBlockRotation;
+    //the expected rotation of the right block
+    public float rightBlockRotation;
+    //threshold for accepting block rotation
+    public float threshold = 5;
+
+    //temp. how much to move each block
+    public float moveDistance;
+
+
+    private SnapRotate leftRotate;
+    private SnapRotate rightRotate;
+    // How many keys have been collected
+    private int keyPieceCount = 0;
+
+
+
+    void Start()
+    {
+        // Add event listeners
+        leftBlock.GetComponentInChildren<XRSocketInteractor>().onSelectEntered.AddListener(OnKeyPieceLocked);
+        rightBlock.GetComponentInChildren<XRSocketInteractor>().onSelectEntered.AddListener(OnKeyPieceLocked);
+
+        leftRotate = leftBlock.GetComponent<SnapRotate>();
+        rightRotate = rightBlock.GetComponent<SnapRotate>();
+    }
+
+
+    // Called when a key piece is placed in a slot
+    private void OnKeyPieceLocked( XRBaseInteractable interactable)
+	{
+        // Change layer mask from Interactable and Key Piece to only Key Piece to prevent the player from picking it back up, while letting the socket keep it
+        interactable.interactionLayerMask = LayerMask.GetMask("Key Piece");
+
+        keyPieceCount++;
+        // Check when both key pieces have been placed
+        if (keyPieceCount == 2)
+        {
+            OnKeysPlaced();
+        }
+    }
+
+    // Called when both key pieces have been placed
+    private void OnKeysPlaced()
+	{
+        // Enable spinning for the blocks
+        leftBlock.GetComponent<Rigidbody>().isKinematic = false;
+        leftRotate.enabled = true;
+        leftBlock.GetComponent<XRGrabInteractable>().interactionLayerMask = LayerMask.GetMask("Interactable");
+        rightBlock.GetComponent<Rigidbody>().isKinematic = false;
+        rightRotate.enabled = true;
+        rightBlock.GetComponent<XRGrabInteractable>().interactionLayerMask = LayerMask.GetMask("Interactable");
+    }
+
+    // Called when the blocks have been rotated to the correct positions
+    private IEnumerator OnBlocksRotated()
+    {
+        // Disable spinning the blocks and lock rotation
+        leftBlock.transform.rotation = Quaternion.Euler(leftBlockRotation, 0, 0);
+        leftBlock.GetComponent<Rigidbody>().isKinematic = true;
+        leftBlock.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+        leftRotate.enabled = false;
+        leftBlock.GetComponent<XRGrabInteractable>().interactionLayerMask = 0;
+
+        rightBlock.transform.rotation = Quaternion.Euler(rightBlockRotation, 0, 0);
+        rightBlock.GetComponent<Rigidbody>().isKinematic = true;
+        rightBlock.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+        rightRotate.enabled = false;
+        rightBlock.GetComponent<XRGrabInteractable>().interactionLayerMask = 0;
+
+
+        // Get the initial positions of the blocks
+        Vector3 leftStart = leftBlock.transform.position;
+        Vector3 rightStart = rightBlock.transform.position;
+
+        float t = 0;
+        while (t < 1)
+		{
+            // Move the blocks together over time
+            leftBlock.transform.position = leftStart + new Vector3(0, 0, moveDistance * (t / 1));
+            rightBlock.transform.position = rightStart - new Vector3(0, 0, moveDistance * (t / 1));
+            
+            t += Time.deltaTime;
+            yield return null;
+		}
+
+
+        // Destroy the key piece prefabs
+        Destroy(leftBlock.GetComponentInChildren<XRSocketInteractor>().selectTarget.gameObject);
+        Destroy(rightBlock.GetComponentInChildren<XRSocketInteractor>().selectTarget.gameObject);
+        // Disable the socket interactors
+        leftBlock.GetComponentInChildren<XRSocketInteractor>().socketActive = false;
+        rightBlock.GetComponentInChildren<XRSocketInteractor>().socketActive = false;
+        
+        //instantiate complete key in a socket between the blocks
+        completeKeySocket.socketActive = true;
+        Instantiate(completeKeyPrefab, completeKeySocket.transform.position, completeKeySocket.transform.rotation);
+    }
+
+
+
+    void Update()
+	{
+        // Both keys have been placed, so the blocks are rotatable
+		if (keyPieceCount == 2)
+		{
+            // Get the rotation of the blocks
+            float leftRot = leftRotate.GetRotation();
+            float rightRot = rightRotate.GetRotation();
+
+            // Check if the blocks are at the correct rotation
+            if (leftRot > leftBlockRotation - threshold && leftRot < leftBlockRotation + threshold &&
+                rightRot > rightBlockRotation - threshold && rightRot < rightBlockRotation + threshold)
+			{
+                keyPieceCount++;
+                StartCoroutine(OnBlocksRotated());
+            }
+		}
+	}
+
+
+    public void OnCompleteKeyTaken()
+	{
+        completeKeySocket.socketActive = false;
+	}
+}
---
+++ OculusQuest2/Assets/Scripts/SnapRotate.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class SnapRotate : MonoBehaviour
+{
+    public enum Axis
+	{
+        X,
+        Y,
+        Z
+	}
+
+    [Tooltip("The axis the object should rotate on")]
+    public Axis rotationAxis;
+
+    [Tooltip("How many stable points there should be")]
+    public int divisions;
+    [Tooltip("Multiplies the amount that the object is rotated")]
+    public float correctionStrength = 5;
+    [Tooltip("Multiplies the amount of conter torque to avoid over rotation")]
+    public float counterTorqueStrength = 2;
+
+
+    private Vector3 axis;
+    private Vector3 zeroDirection;
+    private float divisionSize;
+    private Rigidbody rb;
+
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        divisionSize = 360.0f / divisions;
+
+        // Set direction vectors according to selected axis
+        switch (rotationAxis)
+		{
+            case Axis.X:
+                axis = Vector3.right;
+                zeroDirection = Vector3.up;
+                break;
+            case Axis.Y:
+                axis = Vector3.up;
+                zeroDirection = Vector3.forward;
+                break;
+            case Axis.Z:
+                axis = Vector3.forward;
+                zeroDirection = Vector3.up;
+                break;
+		}
+    }
+
+    void FixedUpdate()
+    {
+        // Find the 'forward' direction by the selected axis
+        Vector3 forward;
+        switch (rotationAxis)
+		{
+            case Axis.X:
+                forward = transform.up;
+                break;
+            case Axis.Y:
+                forward = transform.forward;
+                break;
+            case Axis.Z:
+                forward = transform.up;
+                break;
+        
+            default:
+                forward = Vector3.up;
+                break;
+        }
+
+        // Get the current rotation along the axis
+        float angle = Vector3.SignedAngle(zeroDirection, forward, axis) + 180 + divisionSize * 0.5f;
+        // Find the amount that the hand is off from a stable point as a scalar
+        float diff = divisionSize * 0.5f - (angle % divisionSize);
+        diff /= (divisionSize * 0.5f);
+        // Use the angular velocity along the rotation axis to prevent over rotating
+        float counterTorque = Vector3.Dot(rb.angularVelocity, axis) * counterTorqueStrength * (1.2f - diff);
+
+        // Calculate and apply torque
+        Vector3 torque = axis * (diff * correctionStrength - counterTorque);
+        rb.AddTorque(torque * Time.fixedDeltaTime);
+    }
+
+    // Get this objects rotation along the rotation axis
+    public float GetRotation()
+	{
+        // Find the 'forward' direction by the selected axis
+        Vector3 forward;
+        switch (rotationAxis)
+        {
+            case Axis.X:
+                forward = transform.up;
+                break;
+            case Axis.Y:
+                forward = transform.forward;
+                break;
+            case Axis.Z:
+                forward = transform.up;
+                break;
+
+            default:
+                forward = Vector3.up;
+                break;
+        }
+
+        // Get the angle, looping it so 0 is at zeroDirection and going clockwise
+        float angle = Vector3.SignedAngle(zeroDirection, forward, axis);
+        if (angle < 0)
+		{
+            angle += 360;
+		}
+
+        return angle;
+    }
+}
