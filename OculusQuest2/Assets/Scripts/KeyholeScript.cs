401e5e1d559d27d22a354cce10450ffcaac7dd93
TheTastyGravy <thomasdufresne23@gmail.com>
2021-06-09 20:33:36 +1000

    Add keyhole to test scene
    The keyhole above the key assembler has a socket that will take the key
    and enable it to be turned. After turning it, the key is locked and the
    case door is opened.

--- OculusQuest2/Assets/Scripts/KeyholeScript.cs
+++ OculusQuest2/Assets/Scripts/KeyholeScript.cs
@@ -8,11 +8,18 @@ public class KeyholeScript : MonoBehaviour
 {
     // Where the key should be after being placed
     public Transform keyLockTransform;
-
+   
     public UnityEvent onUnlocked;
+    [Space]
+    // Door to open
+    public Transform caseDoor;
+    // How long it should take to open
+    public float time;
+    // How much to open it by
+    public float angle;
 
-    XRSocketInteractor socket;
-    GameObject key;
+    private XRSocketInteractor socket;
+    private GameObject key;
 
 
 
@@ -30,16 +37,13 @@ public class KeyholeScript : MonoBehaviour
         socket.socketActive = false;
 
         key = interactable.gameObject;
-
-        //dont use gravity, and freeze everything except rotation on the x axis
+        // Freeze everything except rotation on the x axis
         Rigidbody rigid = key.GetComponent<Rigidbody>();
         rigid.useGravity = false;
         rigid.constraints = RigidbodyConstraints.FreezePosition | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
-
         // Move the key to the correct position
         key.transform.position = keyLockTransform.position;
         key.transform.rotation = keyLockTransform.rotation;
-
         // Enable the lock rotation script and add listener for when the key is rotated to the end
         LockRotation rotScript = key.GetComponent<LockRotation>();
         rotScript.enabled = true;
@@ -49,14 +53,32 @@ public class KeyholeScript : MonoBehaviour
     // Called when the key has been turned
     private void OnKeyTurned()
 	{
-        // Disable interaction on the key
+        // Disable interaction on the key and stop it from moving
         key.GetComponent<XRGrabInteractable>().interactionLayerMask = 0;
         Rigidbody rigid = key.GetComponent<Rigidbody>();
         rigid.isKinematic = true;
-        rigid.angularVelocity = Vector3.zero;
+        rigid.constraints = RigidbodyConstraints.FreezeAll;
         key.GetComponent<LockRotation>().enabled = false;
 
         // Invoke Event
         onUnlocked.Invoke();
+        // Open the door
+        StartCoroutine(OpenCaseDoor());
+    }
+
+
+    private IEnumerator OpenCaseDoor()
+	{
+        Quaternion startRot = caseDoor.rotation;
+
+        float t = 0;
+        while (t < time)
+		{
+            // Rotate the case door over time
+            caseDoor.rotation = startRot * Quaternion.Euler(0, 0, angle * (t / time));
+
+            t += Time.deltaTime;
+            yield return null;
+		}
 	}
 }
