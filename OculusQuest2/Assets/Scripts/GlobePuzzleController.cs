0067cf08f44b86057c20fe1721911890a43390a8
Logan Ryan <loganryan982@gmail.com>
2021-06-11 13:32:48 +1000

    Add functionality to globe puzzle

---
+++ OculusQuest2/Assets/Scripts/GlobePuzzleController.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobePuzzleController : MonoBehaviour
+{
+    /*
+      - Reset the puzzle if the player pushes the wrong button
+        - Check which button has been pushed
+        - Check the order number of the button that has been pushed
+        - if it is not the first one then reset the puzzle
+        - if the first button has been pushed then leave it down
+        - if the player gets the 2nd button incorrect then reset the puzzle
+    */
+    //private HandButton[] globeButtons;
+    private List<HandButton> globeButtons = new List<HandButton>();
+
+    [HideInInspector]
+    public HandButton buttonPushed;
+    
+    private int correctAnswers = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (buttonPushed != null && buttonPushed.arrived)
+        {
+            HandButton.ORDER buttonNumber = HandButton.ORDER.First;
+            
+            switch (correctAnswers)
+            {
+                case 1:
+                    buttonNumber = HandButton.ORDER.Second;
+                    break;
+                case 2:
+                    buttonNumber = HandButton.ORDER.Third;
+                    break;
+                case 3:
+                    buttonNumber = HandButton.ORDER.Fourth;
+                    break;
+                case 4:
+                    buttonNumber = HandButton.ORDER.Fifth;
+                    break;
+                default:
+                    break;
+            }
+
+            buttonPushed.arrived = false;
+            StartCoroutine(CheckButton(buttonNumber));
+        }
+    }
+
+    IEnumerator CheckButton(HandButton.ORDER buttonNumber)
+    {
+        yield return new WaitForSeconds(2);
+
+        if (buttonPushed != null && buttonPushed.orderNumber == buttonNumber)
+        {
+            correctAnswers++;
+            globeButtons.Add(buttonPushed);
+            Debug.Log("Correct Answer");
+        }
+        else if (buttonPushed != null)
+        {
+            Debug.Log("Incorrect Answer. Resetting Puzzle!");
+            buttonPushed.ResetButton();
+
+            foreach (var item in globeButtons)
+            {
+                item.ResetButton();
+            }
+
+            globeButtons.Clear();
+
+            correctAnswers = 0;
+        }
+
+        buttonPushed = null;
+    }
+}
--- OculusQuest2/Assets/Scripts/HandButton.cs
+++ OculusQuest2/Assets/Scripts/HandButton.cs
@@ -1,22 +1,80 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Events;
-using UnityEngine.XR.Interaction.Toolkit;
+﻿using UnityEngine;
 
 public class HandButton : MonoBehaviour
 {
-    private bool pressed = false;
+    public enum ORDER
+    {
+        First = 0,
+        Second,
+        Third,
+        Fourth,
+        Fifth
+    }
+    
+    public float speed = .5f;
+    public Vector3 depth;
+    public ORDER orderNumber;
+    public GlobePuzzleController globePuzzleController;
 
-    public Vector3 endPosition;
+    [HideInInspector]
+    public bool arrived = false;
+
+    private float fraction = 0;
+    private bool pressed = false;
+    private bool reset = false;
+    private Vector3 startPosition;
+    private Vector3 endPosition;
+
+    private void Start()
+    {
+        startPosition = transform.position;
+        endPosition = transform.position - depth;
+    }
+
+    private void Update()
+    {
+        if (pressed && !reset)
+        {
+            if (fraction < 1)
+            {
+                fraction += Time.deltaTime * speed;
+                transform.position = Vector3.Lerp(startPosition, endPosition, fraction);
+            }
+            else
+            {
+                arrived = true;
+            }
+        }
+
+        if (reset)
+        {
+            if (fraction < 1)
+            {
+                fraction += Time.deltaTime * speed;
+                transform.position = Vector3.Lerp(endPosition, startPosition, fraction);
+            }
+            else 
+            {
+                fraction = 0;
+                reset = false;
+                pressed = false;
+            }
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (!pressed && other.gameObject.name == "Index")
+        if (!pressed && other.gameObject.name == "Index" && globePuzzleController.buttonPushed == null)
         {
             pressed = true;
-
-            transform.localPosition = endPosition;
+            globePuzzleController.buttonPushed = this;
         }
+    }
+
+    // Return the button to the original position
+    public void ResetButton()
+    {
+        reset = true;
+        fraction = 0;
     }
 }
