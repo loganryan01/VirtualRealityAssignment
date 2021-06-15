02b17da684307cc542708b43c72523b2b302d17e
TheTastyGravy <thomasdufresne23@gmail.com>
2021-06-15 14:59:08 +1000

    Add ending scene
    The ending scene plays an audio clip, displays some text, then returns
    to the main menu.

---
+++ OculusQuest2/Assets/Scripts/EndingScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndingScript : MonoBehaviour
+{
+    public Text endingText;
+
+    public float delayForText;
+    public float fadeTime;
+    public float delayForMenu;
+
+
+
+    void Start()
+    {
+        StartCoroutine(FadeText());
+    }
+
+    private IEnumerator FadeText()
+	{
+        yield return new WaitForSeconds(delayForText);
+
+        // Fade text in
+        float t = 0;
+        while (t < fadeTime)
+		{
+            endingText.color = Color.Lerp(Color.clear, Color.white, t / fadeTime);
+
+            t += Time.deltaTime;
+            yield return null;
+		}
+
+
+        yield return new WaitForSeconds(delayForMenu);
+        // Return to menu
+        TransitionManager.instance.ChangeScene(1);
+	}
+}
